170124bc0ee7aa77ae85e932bb53f158f3322ba2
GilesR6 <>
2021-04-05 11:33:51 +0000

    added brackets to iputs

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #write your code below this line
+    first = input('Give a string:')
+    second = input('Give an interger:')
+    third = input('Give a float:')
+    forth = input('Give a boolean:')
 
+    print ("You gave a string:" + first)
+    print ("You gave an interger:" + second)
+    print ("You gave a float:" + third)
+    print ("You gave the boolean:" + forth)
 if __name__ == '__main__':
     main()
